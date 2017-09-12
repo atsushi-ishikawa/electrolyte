20f1f35cd2768751ed7b57a60ecbe1a6fb8f6e50
Atsushi Ishikawa <ishikawasuteado@gmail.com>
2017-09-12 21:04:02 +0900

    adjusted ion solvation

--- ion.py
+++ ion.py
@@ -5,6 +5,8 @@ from ase.db import connect
 from ase.optimize import BFGS
 from ase.io import read, write
 
+from ase.visualize import view
+
 from tools import delete_num_from_json
 
 import os, sys
@@ -15,14 +17,14 @@ argvs = sys.argv
 nions = len(argvs) - 2 # number of ion species
 
 # get ion list
-for i in range(0,nions):
+for i in range(0, nions):
 	ions.append(str(argvs[i+1]))
 
 print "ions", ions
 
 num = int(argvs[nions + 1])
 
-solv_jsonfile     = "electrolye_2017Aug.json"
+solv_jsonfile = "electrolye_2017Aug.json"
 
 # ---
 xc    = "B3LYP"
@@ -64,15 +66,89 @@ for ion in ions:
 		else:
 			print "only Li, Na, Mg is allowed" ; quit()
 
+		print "smiles", smiles
+		if "(O=" in smiles or "=O)" in smiles:
+			print "case 1"
+			ion_smiles = "" + smiles + ""
+			replace_str1 = "(([" + ion + "])O="
+			replace_str2 = "=O([" + ion + "]))"
+			ion_smiles = ion_smiles.replace("(O=", replace_str1,1)
+			ion_smiles = ion_smiles.replace("=O)", replace_str2,1)
+		elif "O=" in smiles or "=O" in smiles:
+			print "case 2"
+			ion_smiles = "" + smiles + ""
+			replace_str1 = "([" + ion + "])O="
+			replace_str2 = "=O([" + ion + "])"
+			ion_smiles = ion_smiles.replace("O=", replace_str1,1)
+			ion_smiles = ion_smiles.replace("=O", replace_str2,1)
+		elif "[O]" in smiles:
+ 			print "case 3c"
+ 			ion_smiles = "" + smiles + ""
+ 			replace_str1 = "[O]([" + ion + "])"
+ 			ion_smiles = ion_smiles.replace("[O]", replace_str1,1)
+		elif "O1" in smiles:
+			if smiles.startswith("O1"):
+				print "case 3-A"
+				ion_smiles = smiles
+				replace_str1 = "[" + ion + "]O1"
+		 		#replace_str1 = "O1"
+				ion_smiles = ion_smiles.replace("O1", replace_str1,1)
+			else:
+				print "case 3-a"
+				ion_smiles = smiles
+				replace_str1 = "O1([" + ion + "])"
+		 		#replace_str1 = "O1"
+				ion_smiles = ion_smiles.replace("O1", replace_str1,1)
+		elif "o1" in smiles:
+			print "case 3-b"
+			ion_smiles = smiles
+			#replace_str1 = "[" + ion + "](o1)"
+			replace_str1 = "o1([" + ion + "])"
+			ion_smiles = ion_smiles.replace("o1", replace_str1,1)
+		elif "O" in smiles:
+ 			print "case 3d"
+ 			ion_smiles = "" + smiles + ""
+ 			#replace_str1 = "[" + ion + "]O"
+ 			replace_str1 = "O([" + ion + "])"
+ 			ion_smiles = ion_smiles.replace("O", replace_str1,1)
+		elif "C#N" in smiles:
+			print "case 4"
+			ion_smiles = smiles
+			replace_str1 = "C#N([" + ion + "])"
+			ion_smiles = ion_smiles.replace("C#N", replace_str1,1)
+		elif "n1" in smiles:
+			print "case 5-a"
+			ion_smiles = smiles
+			replace_str1 = "n1([" + ion + "])"
+			ion_smiles = ion_smiles.replace("n1", replace_str1,1)
+		elif "n2" in smiles:
+			print "case 5-b"
+			ion_smiles = smiles
+			replace_str1 = "n2([" + ion + "])"
+			ion_smiles = ion_smiles.replace("n2", replace_str1,1)
+		elif "S" in smiles:
+			print "case 6-a"
+			ion_smiles = smiles
+			replace_str1 = "S([" + ion + "])"
+			ion_smiles = ion_smiles.replace("S", replace_str1,1)
+		elif "s1" in smiles:
+			print "case 6-b"
+			ion_smiles = smiles
+			replace_str1 = "s1([" + ion + "])"
+			ion_smiles = ion_smiles.replace("s1", replace_str1,1)
+		else:
+			print "case 10"
+			ion_smiles = "(" + smiles + ")"
+			ion_smiles = ion_smiles + "[" + ion + "]"
 
-		print ion, "coordinated"
-
-		replace_str1 = "([" + ion + "][O]="
-		replace_str2 = "=[O][" + ion + "])"
-
-		ion_smiles = "(" + smiles + ")"
-		ion_smiles = ion_smiles.replace("(O=", replace_str1, 1) # replace just one
-		ion_smiles = ion_smiles.replace("=O)", replace_str2, 1) # 
+#		print ion, "coordinated"
+#
+#		replace_str1 = "([" + ion + "][O]="
+#		replace_str2 = "=[O][" + ion + "])"
+#
+#		ion_smiles = "(" + smiles + ")"
+#		ion_smiles = ion_smiles.replace("(O=", replace_str1, 1) # replace just one
+#		ion_smiles = ion_smiles.replace("=O)", replace_str2, 1) # 
 
 		print "ion_smiles", ion_smiles
 
@@ -86,17 +162,19 @@ for ion in ions:
 		print "found in database"
 		delete_num_from_json(num, solv_ion_jsonfile)
 
-	label = "calc" + ion + str(num).zfill(4) + "/nwchem_low"
+	view(ion_solv)
 
-	ion_solv.calc = NWChem(label=label, xc=xc, basis=basis, charge=ion_charge, mult=1, 
-						   iterations=200, mulliken=True) # cation
-                       
-	traj = ion + "_low_" + str(num).zfill(4) + ".traj"
-	BFGS(ion_solv, trajectory=traj).run(fmax=fmax)
 
-	db_ion.write(ion_solv, smiles=ion_smiles, name=name, num=num, 
-					molecular_weight=wgt, density=dens,
-					boiling_point=bp, melting_point=mp, flushing_point=fp,
-				  	level="low"
-			)
+#	label = "calc" + ion + str(num).zfill(4) + "/nwchem_low"
+#	ion_solv.calc = NWChem(label=label, xc=xc, basis=basis, charge=ion_charge, mult=1, 
+#						   iterations=200, mulliken=True) # cation
+#                       
+#	traj = ion + "_low_" + str(num).zfill(4) + ".traj"
+#	BFGS(ion_solv, trajectory=traj).run(fmax=fmax)
+#
+#	db_ion.write(ion_solv, smiles=ion_smiles, name=name, num=num, 
+#					molecular_weight=wgt, density=dens,
+#					boiling_point=bp, melting_point=mp, flushing_point=fp,
+#				  	level="low"
+#			)
 
